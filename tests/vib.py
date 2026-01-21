44e1d36d271857a2b3e37d8c988ae1219a6cd4bf
benrich37 <benjamin.rich@colorado.edu>
2026-01-21 14:57:55 -0700

    Test check - base function currently fails this

--- tests/vib.py
+++ tests/vib.py
@@ -1,6 +1,33 @@
 import pytest
 from JDFTxFreeNrg.qrrho import get_qrrho_vib_entropies, get_qrrho_vib_enthalpies
 from JDFTxFreeNrg.standard import get_enthalpy_vib, get_entropy_vib, k_ev
+from JDFTxFreeNrg.hessian import pert_along_vec
+
+
+@pytest.mark.parametrize(
+        ("natoms", "disp"),
+        [
+            (5, 0.1),
+        ]
+)
+def test_pert_along_vec(natoms: int, disp: float):
+    from pymatgen.core import Structure
+    import numpy as np
+    coords = np.random.random((natoms, 3))
+    struc = Structure(np.eye(3)*10., list(["H" for _ in range(natoms)]), coords)
+    vec = np.random.random((natoms, 3))*5
+    pert_struc = pert_along_vec(struc, vec, disp)
+    pert_coords = pert_struc.cart_coords
+    dvec = pert_coords - coords
+    dvec_len = np.linalg.norm(dvec)
+    assert np.shape(pert_coords) == (natoms, 3)
+    # Check magnitude is correct
+    assert np.isclose(np.linalg.norm(dvec), disp), f"Expected displacement {disp}, got {dvec_len}"
+    # Check displacement direction is correct
+    dvec_normed = dvec / dvec_len
+    vec_norm = vec / np.linalg.norm(vec)
+    assert np.allclose(dvec_normed, vec_norm)
+
 
 
 @pytest.mark.parametrize(
