12f4102b168bd81ed82710bb45efc686b26b7431
benrich37 <benjamin.rich@colorado.edu>
2026-02-11 13:23:39 -0700

    Default searched_E to a value I found to be at convergence for free getting the free energy

--- src/JDFTxFreeNrg/qrotor.py
+++ src/JDFTxFreeNrg/qrotor.py
@@ -16,7 +16,7 @@ def load_man(
     return 
 
 
-def get_system(energies: list[float], angles:list[float], inertia: float, searched_E: int | None = None) -> System:
+def get_system(energies: list[float], angles:list[float], inertia: float, searched_E: int = 400) -> System:
     inertia_SI = inertia * const.physical_constants['atomic mass constant'][0] * 1e-20
     system_B = (const.physical_constants['reduced Planck constant'][0]**2) / (2 * inertia_SI) * (1000 / const.eV)
     system = load_man(energies, angles)
